7b3cfdf6a1e513f82d1a561ae02c7d9ffc6fc8c4
hung292003 <nhh292003@gmail.com>
2023-08-16 19:18:52 +0700

    add base project

---
+++ src/test/java/com/spring/entities/VaccinationSrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationSrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
