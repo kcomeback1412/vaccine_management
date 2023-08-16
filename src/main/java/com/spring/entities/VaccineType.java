e40a1bb379aa16ece5e88efc7d8d886d3855ebc1
hung292003 <nhh292003@gmail.com>
2023-08-16 20:27:43 +0700

    add entities P2

--- src/main/java/com/spring/entities/VaccineType.java
+++ src/main/java/com/spring/entities/VaccineType.java
@@ -1,4 +1,23 @@
 package com.spring.entities;
 
-public class VaccineType {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+public class VaccineType implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "VACCINE_TYPE_ID", length = 36)
+    private String vaccineTypeId;
+
+    @Column(name = "DESCRIPTION", length = 200)
+    private String description;
+
+    @Column(name = "VACCINE_TYPE_Name", length = 200)
+    private String indication;
 }
