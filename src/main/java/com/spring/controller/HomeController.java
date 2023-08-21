2a1a8149e0449c460a718596ac862d3cae41f181
Nguyễn Hồng Hưng <nhh29203@gmail.com>
2023-08-21 12:59:00 +0000

    Dev

--- src/main/java/com/spring/controller/HomeController.java
+++ src/main/java/com/spring/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping(value = {"","/","/index","/home","dashboard"})
+    public String homePageUI(Model model){
+        return "index";
+    }
+
 }
