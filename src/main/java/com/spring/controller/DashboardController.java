f82c4f05373df35bda97591c443c7fd1d0653296
Nguyễn Hồng Hưng <nhh29203@gmail.com>
2023-08-23 14:38:31 +0000

    Dev

--- src/main/java/com/spring/controller/DashboardController.java
+++ src/main/java/com/spring/controller/DashboardController.java
@@ -15,6 +15,21 @@ public class DashboardController {
     	return "employee/employee_list";
     }
     
+    @GetMapping("/schedule_list")
+    public String scheduleList() {
+    	return "schedule/schedule_list";
+    }
+    
+    @GetMapping("/vaccine_type_list")
+    public String vaccineTypeList() {
+    	return "vaccine/vaccine_type_list";
+    }
+    
+    @GetMapping("/create_customer")
+    public String createCustomer() {
+    	return "customer/create_customer";
+    }
+    
     @GetMapping("/login")
     public String loginUI() {
     	return "login";
