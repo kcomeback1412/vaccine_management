4c7c0f5eacb7df81b0e2685499f6e0df51a10c12
name <email@123.com>
2023-08-25 20:55:56 +0700

    Thêm trang updateVaccineType và trang updateInjectionResult

--- src/main/java/com/spring/controller/VaccineTypeController.java
+++ src/main/java/com/spring/controller/VaccineTypeController.java
@@ -7,7 +7,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.consts.StatusEnum;
 import com.spring.entities.VaccineType;
@@ -32,12 +34,17 @@ public class VaccineTypeController {
     }
 	
 	@PostMapping("/addVaccineType")
-    public String createVaccineType(
+    public String addVaccineType(
     		@ModelAttribute("vaccineType") @Validated VaccineType vaccineType,
     		BindingResult result,
     		Model model) {
 		
 		if(result.hasErrors()) {
+			return "vaccineType/addVaccineType";
+		}
+		
+		if(null != vaccineTypeRepository.checkVaccineTypeId(vaccineType.getVaccineTypeId())) {
+			model.addAttribute("vaccineTypeIdMsg", "This code is already existed!");
 			return "vaccineType/addVaccineType";
 		}
 		
@@ -48,8 +55,46 @@ public class VaccineTypeController {
     	return "redirect:/addVaccineType";
     }
 	
-	@GetMapping("/updateVaccineType")
-    public String updateVaccineTypeUI() {
+	@GetMapping("/updateVaccineType/{id}")
+    public String updateVaccineTypeUI(
+    		@PathVariable String id, 
+    		Model model) {
+		
+		model.addAttribute("vaccineType", vaccineTypeRepository.findById(id));
+		
+		if(vaccineTypeRepository.findById(id).orElse(null).getVaccineTypeStatus() == StatusEnum.ACTIVE) {
+			
+		}
+		
     	return "vaccineType/updateVaccineType";
+    }
+	
+	@GetMapping("/updateVaccineType")
+    public String updateVaccineType(Model model) {
+		
+		model.addAttribute("vaccineType", new VaccineType());
+		
+    	return "vaccineType/updateVaccineType";
+    }
+	
+	@PostMapping("/updateVaccineType")
+    public String updateVaccineType(
+    		@ModelAttribute("vaccineType") @Validated VaccineType vaccineType,
+    		BindingResult result,
+    		@RequestParam("activeCheckBox") boolean active,
+    		Model model) {
+		
+		if(result.hasErrors()) {
+			return "vaccineType/updateVaccineType";
+		}
+		
+		if(active) 
+			vaccineType.setVaccineTypeStatus(StatusEnum.ACTIVE);
+		else 
+			vaccineType.setVaccineTypeStatus(StatusEnum.IN_ACTIVE);
+		
+		vaccineTypeRepository.save(vaccineType);
+		
+    	return "redirect:/updateVaccineType";
     }
 }
