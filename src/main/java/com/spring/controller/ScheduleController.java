b26d56a8571bbca5a74546f9c2fe86e18518ab45
Nguyễn Hồng Hưng <nhh29203@gmail.com>
2023-08-28 11:10:01 +0000

    Dev

--- src/main/java/com/spring/controller/ScheduleController.java
+++ src/main/java/com/spring/controller/ScheduleController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ScheduleController {
 
-	@GetMapping("/schedule-list")
-    public String scheduleList() {
-    	return "schedule/schedule-list";
-    }
+//	@GetMapping("/schedule-list")
+//    public String scheduleList() {
+//    	return "schedule/schedule-list";
+//    }
 }
