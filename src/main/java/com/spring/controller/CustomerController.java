4825a485ca6d998d2424786b034fa63815716297
Admin <Admin@Admin>
2023-08-29 14:25:19 +0700

    commit

--- src/main/java/com/spring/controller/CustomerController.java
+++ src/main/java/com/spring/controller/CustomerController.java
@@ -1,16 +1,20 @@
 package com.spring.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.spring.entities.UserDetail;
 import com.spring.entities.Users;
 import com.spring.repositories.UserDetailRepository;
 import com.spring.repositories.UsersRepository;
-
 @Controller
 public class CustomerController {
 	
@@ -21,10 +25,33 @@ public class CustomerController {
 	UserDetailRepository userDetailRepository;
 
     @GetMapping("/customer_list")
-    public String CustomerList() {
-    	
-    	
+    public String CustomerList(
+    		Model model
+    		) {
+    	List<UserDetail> customer = userDetailRepository.findAll();    	
+    	model.addAttribute("customerList", customer);
         return "customer/customer_list";
+    }
+    
+    @PostMapping("/create-customer")
+    public String createCustomer(
+    		@ModelAttribute("customerInfo") Users customer,
+    		@ModelAttribute("userInfo") UserDetail account
+    		) {
+    	String id = UUID.nameUUIDFromBytes(customer.getUserName().getBytes()).toString();
+    	customer.setUsersId(id);
+    	usersRepository.save(customer);
+    	account.setUsers2(customer);
+    	userDetailRepository.save(account);
+    	System.out.println(customer.toString()+ "+" + account.toString());
+    	return "redirect:/customer_list";
+    }
+    
+    @GetMapping("/delete-customer/{id}")
+    public String deleteCustomer(@PathVariable("id") String id ){
+    	
+    	userDetailRepository.deleteById(Integer.parseInt(id));
+    	return "customer/customer_list";
     }
 
     @GetMapping("/update-customer")
@@ -37,17 +64,6 @@ public class CustomerController {
         return "customer/create-customer";
     }
     
-    @PostMapping("/create-customer")
-    public String createCustomer(
-    		@ModelAttribute("customerInfo") Users customer,
-    		@ModelAttribute("userInfo") UserDetail account
-    		) {
-    	customer.setUsersId("1213");
-    	usersRepository.save(customer);
-    	account.setUsers2(customer);
-    	userDetailRepository.save(account);
-    	System.out.println(customer.toString()+ "+" + account.toString());
-    	return "redirect:/customer_list";
-    }
+  
     
 }
