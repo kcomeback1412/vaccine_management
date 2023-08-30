da4d2c95acfbc16695243d8faf9f3687324864f8
Admin <Admin@Admin>
2023-08-30 11:39:07 +0700

    commit

--- src/main/java/com/spring/controller/CustomerController.java
+++ src/main/java/com/spring/controller/CustomerController.java
@@ -19,57 +19,64 @@ import com.spring.repositories.UserDetailRepository;
 import com.spring.repositories.UsersRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class CustomerController {
-	
+
 	@Autowired
 	UsersRepository usersRepository;
-	
+
 	@Autowired
 	UserDetailRepository userDetailRepository;
 
-    @GetMapping("/customer_list")
-    public String CustomerList(
-    		Model model
-    		) {
-    	List<UserDetail> customer = userDetailRepository.findAll();    	
-    	model.addAttribute("customerList", customer);
-        return "customer/customer_list";
-    }
-    
-    @PostMapping("/create-customer")
-    public String createCustomer(
-    		@ModelAttribute("customerInfo") Users customer,
-    		@ModelAttribute("userInfo") UserDetail account
-    		) {
-    	String id = UUID.nameUUIDFromBytes(customer.getUserName().getBytes()).toString();
-    	customer.setUsersId(id);
-    	usersRepository.save(customer);
-    	account.setUsers2(customer);
-    	userDetailRepository.save(account);
-    	System.out.println(customer.toString()+ "+" + account.toString());
-    	return "redirect:/customer_list";
-    }
-    
-    @RequestMapping(value = "delete", method = RequestMethod.POST)
-    public String deleteCustomer(HttpServletRequest request){
-    	for (String id : request.getParameterValues("id")) {
-			userDetailRepository.deleteById(Integer.parseInt(id));
+	@GetMapping("/customer_list")
+	public String CustomerList(Model model) {
+		List<UserDetail> customer = userDetailRepository.findAll();
+		model.addAttribute("customerList", customer);
+		return "customer/customer_list";
+	}
+
+	@PostMapping("/create-customer")
+	public String createCustomer(@ModelAttribute("customerInfo") Users customer,
+			@ModelAttribute("userInfo") UserDetail account) {
+		String id = UUID.nameUUIDFromBytes(customer.getUserName().getBytes()).toString();
+		customer.setUsersId(id);
+		usersRepository.save(customer);
+		account.setUsers2(customer);
+		userDetailRepository.save(account);
+		System.out.println(customer.toString() + "+" + account.toString());
+		return "redirect:/customer_list";
+	}
+	
+	@RequestMapping(value="/update-delete",params="delete",method=RequestMethod.POST)
+	public String findCustomer(
+			HttpServletRequest httpServletRequest
+			) {
+		try {
+			if(httpServletRequest.getParameterValues("id") != null) {
+				for (String id : httpServletRequest.getParameterValues("id")) {
+					userDetailRepository.deleteById(Integer.parseInt(id));
+				}
+			}
+			return "redirect:/customer_list";
+		} catch (Exception e) {
+			return "customer/customer_list";
 		}
-    	
-    	return "customer/customer_list";
-    }
+	}
+	
+	@RequestMapping(value="/update-delete",params="create",method=RequestMethod.POST)
+	public String customerCreatePage() {
+		return "customer/create-customer";
+	}
 
-    @GetMapping("/update-customer")
-    public String updateCustomerUI() {
-        return "customer/update-customer";
-    }
+	@RequestMapping(value="/update-delete",params="update",method=RequestMethod.POST)
+	public String updateCustomerUI() {
+		return "customer/update-customer";
+	}
 
-    @GetMapping("/create-customer")
-    public String createCustomerUI() {
-        return "customer/create-customer";
-    }
-    
-  
-    
+	@GetMapping("/create-customer")
+	public String createCustomerUI() {
+		return "customer/create-customer";
+	}
+
 }
