535c9dce0e763a544a9861be08d1cda55054e402
Admin <Admin@Admin>
2023-09-02 17:46:08 +0700

    commit

--- src/main/java/com/spring/repositories/UserDetailRepository.java
+++ src/main/java/com/spring/repositories/UserDetailRepository.java
@@ -2,8 +2,13 @@ package com.spring.repositories;
 
 
 import com.spring.entities.UserDetail;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,5 +16,9 @@ public interface UserDetailRepository extends JpaRepository<UserDetail, Integer>
 	
 	@Query("SELECT u FROM UserDetail u WHERE u.id = ?1")
 	public UserDetail findByIdUserDetail(String id);
+	
+	@Query("SELECT u FROM UserDetail u WHERE u.fullName = :name")
+	public Page<UserDetail> findUserDetailWithPagin(@Param("name") String name,  Pageable pageable);
+	
 	
 }
