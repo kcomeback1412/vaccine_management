c9240e74cef9d781b247728595c6b2b28c2b3756
Nguyễn Hồng Hưng <nhh29203@gmail.com>
2023-09-05 15:59:49 +0000

    Dev

--- src/main/java/com/spring/repositories/UserDetailRepository.java
+++ src/main/java/com/spring/repositories/UserDetailRepository.java
@@ -1,15 +1,46 @@
 package com.spring.repositories;
 
 
+import com.spring.consts.RoleEnum;
 import com.spring.entities.UserDetail;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserDetailRepository extends JpaRepository<UserDetail, Integer> {
 	
 	@Query("SELECT u FROM UserDetail u WHERE u.id = ?1")
 	public UserDetail findByIdUserDetail(String id);
+
+
+	@Transactional(rollbackOn = {Exception.class, Throwable.class})
+	public List<UserDetail> findAllByUsers2RoleEnum(RoleEnum role);
+
+	@Modifying
+	@Transactional(rollbackOn = {Exception.class, Throwable.class})
+	@Query("DELETE FROM UserDetail AS U WHERE U.id IN (?1)")
+	public void deleteUserDetailByListId(List<Integer> listId) ;
+
+	@Transactional(rollbackOn = {Exception.class, Throwable.class})
+	@Query("SELECT U FROM UserDetail AS U WHERE U.fullName like %?1%")
+	public List<UserDetail> findAllByFullNameLike(String fullName);
+
+	@Transactional(rollbackOn = {Exception.class, Throwable.class})
+	public Integer countAllByUsers2RoleEnum(RoleEnum roleEmployee);
+
+
+	
+	@Query("SELECT u FROM UserDetail u WHERE u.fullName = :name")
+	public Page<UserDetail> findUserDetailWithPagin(@Param("name") String name,  Pageable pageable);
+	
 	
 }
