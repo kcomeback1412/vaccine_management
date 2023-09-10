733c6470acd7e79bc51a9ccb0a52f69ffa70234f
kcomeback1412 <maithanhson0507@gmail.com>
2023-09-10 16:48:26 +0700

    update entities mapping

---
+++ src/main/java/com/spring/service/impl/PlaceServiceImpl.java
@@ -0,0 +1,13 @@
+package com.spring.service.impl;
+
+import com.spring.entities.Users;
+import com.spring.repositories.UsersRepository;
+import com.spring.service.PlaceService;
+import com.spring.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlaceServiceImpl implements PlaceService {
+
+}
